92316168879d2a0a09b1c168c47a7a23bf2c416e
Doc <115489264+Doc004@users.noreply.github.com>
2024-05-08 15:32:56 +0500

    Add files via upload

---
+++ 6/app.js
@@ -0,0 +1,13 @@
+let html = document.querySelector('body')
+html.addEventListener ('click',(bu1) => {
+    function click (){
+            if (html==bu1) {
+        StyleSheet
+
+        
+    }else{
+
+    }
+    }
+
+})
